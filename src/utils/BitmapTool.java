d94ff27d03e6724040197b5b8bfe655b00f5187f
漠川 <mochuan.zzw@alibaba-inc.com>
2014-11-19 14:04:11 +0800

    --

--- src/utils/BitmapTool.java
+++ src/utils/BitmapTool.java
@@ -15,10 +15,12 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PixelFormat;
 import android.graphics.PorterDuff.Mode;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 
 public class BitmapTool {
@@ -78,12 +80,12 @@ public class BitmapTool {
 	}
 
 	/**
-	 * 图片Url保存为位图并进行缩放操作 通过传入图片url获取位图方法
-	 * 
+	 * 图片 Url保存为位图并进行缩放操作 通过传入图片url获取位图方法
+	 * 非异步方法！！
 	 * @param url
 	 * @return
 	 */
-	public Bitmap returnBitMap(String url) {
+	public  static Bitmap returnBitMap(String url) {
 		URL myFileUrl = null;
 		Bitmap bitmap = null;
 		try {
@@ -214,5 +216,51 @@ public class BitmapTool {
 	    canvas.drawRoundRect(rectF, 0, 0, paint);
 	    return output;
 	}
+	
+	/*
+     * Bitmap → byte[]
+     */
+    public static byte[] Bitmap2Bytes(Bitmap bm) {
+        if (bm == null) {
+            return null;
+        }
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        return baos.toByteArray();
+    }
+
+    /*
+     * byte[] → Bitmap
+     */
+    public static Bitmap Bytes2Bimap(byte[] b) {
+        if (b.length == 0) {
+            return null;
+        }
+        return BitmapFactory.decodeByteArray(b, 0, b.length);
+    }
+
+    /*
+     * Drawable → Bitmap
+     */
+    public static Bitmap drawable2Bitmap(Drawable drawable) {
+        if (drawable == null) {
+            return null;
+        }
+        // 取 drawable 的长宽
+        int w = drawable.getIntrinsicWidth();
+        int h = drawable.getIntrinsicHeight();
+        // 取 drawable 的颜色格式
+        Bitmap.Config config = drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888
+                : Bitmap.Config.RGB_565;
+        // 建立对应 bitmap
+        Bitmap bitmap = Bitmap.createBitmap(w, h, config);
+        // 建立对应 bitmap 的画布
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, w, h);
+        // 把 drawable 内容画到画布中
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
 
 }
